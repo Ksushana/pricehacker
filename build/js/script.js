87701609d046fef51b23d6b6c0343e967d947ed1
Ksenia Balashova <utyatka@gmail.com>
2020-10-27 00:55:27 +0100

    fix

--- build/js/script.js
+++ build/js/script.js
@@ -50,7 +50,12 @@
 
 (() => {
   const questions = document.querySelectorAll(`.questions__list-item`);
+
   const btn = document.querySelector(`.questions__btn`);
+
+  if (!btn) {
+    return;
+  }
 
   btn.addEventListener("click", function() {
     for (var i = 0; i < questions.length; i++) {
--- src/js/modules/show-more.js
+++ src/js/modules/show-more.js
@@ -1,6 +1,11 @@
 (() => {
   const questions = document.querySelectorAll(`.questions__list-item`);
+
   const btn = document.querySelector(`.questions__btn`);
+
+  if (!btn) {
+    return;
+  }
 
   btn.addEventListener("click", function() {
     for (var i = 0; i < questions.length; i++) {
